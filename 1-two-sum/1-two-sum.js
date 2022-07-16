4bf7744dcfbd8f1033003b3410cebf70569e2905
arshan97 <42138728+arshan97@users.noreply.github.com>
2022-07-17 02:50:53 +0530

    Time: 77 ms (89.28%), Space: 44.1 MB (16.60%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,13 +4,21 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-const map = {};
+    const map = {};
+    const res = [];
     
-    for(let i = 0; i < nums.length; i++) {
-        if(target - nums[i] in map) {
-            return [map[target - nums[i]], i]
-        } else {
-            map[nums[i]] = i;
+    for (let i = 0; i < nums.length; i++) {
+       const num = nums[i];
+        const complement = target - num;
+        
+        if(map[complement] !== undefined) {
+            res.push(map[complement])
+            res.push(i);
         }
+            
+            map[num] = i;
     }
+        
+        return res;
+    
 };
