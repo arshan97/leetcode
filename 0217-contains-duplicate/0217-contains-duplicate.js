cf84cd8a427517e723106e988368514c906dd421
Arshan Shaikh <42138728+arshan97@users.noreply.github.com>
2023-06-11 01:18:53 +0530

    Time: 81 ms (92.86%), Space: 55.2 MB (36.45%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const numsSet = new Set(nums);
+    const isEqual = numsSet.size !== nums.length;
+    return isEqual;
+};
