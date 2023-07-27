76e743a51e8f1ce97a05853adafa83860a5ea758
Arshan Shaikh <42138728+arshan97@users.noreply.github.com>
2023-07-28 02:39:46 +0530

    Time: 78 ms (99.87%), Space: 56 MB (20.51%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,19 +3,18 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    const arr = []
+    const res = []
     let prefix = 1
     
     for(let i = 0; i<nums.length; i++) {
-        arr.push(prefix)
+        res.push(prefix)
         prefix = prefix*nums[i]
     }
     
-    const res = []
     let postfix = 1
     
     for(let i = nums.length - 1; i>=0; i--) {
-        res.unshift(postfix*arr[i])
+        res[i] = res[i] * postfix
         postfix = postfix*nums[i]
     }
     
